33bfa12901422cf4be0a8a57aad380fb3f377390
CrymSonir <revenu.sylvain@gmail.com>
2020-06-18 12:42:03 +0200

    Fix pdf name

--- amd/src/pimenkoquestionnaire.js
+++ amd/src/pimenkoquestionnaire.js
@@ -1,16 +1,7 @@
 define(['jquery'], function($) {
     let event = function event() {
-        $('.printtopdf').click(function () {
-            let title = $('#coursetitle').val();
-            document.title = window.parent.document.title = title;
-
-            // Other solution.
-            // html2canvas(document.getElementById("region-main")).then(canvas => {
-            //     let pdf = new jsPDF('p', 'mm', 'a4');
-            //     pdf.addImage(canvas.toDataURL('image/png'), 'PNG', 0, 0, 211, 298);
-            //     pdf.save('Test.pdf');
-            // });
-        });
+        let title = $('#coursetitle').val();
+        document.title = window.parent.document.title = title;
     };
 
     return {
